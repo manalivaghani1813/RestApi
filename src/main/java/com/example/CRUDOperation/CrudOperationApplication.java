6f4a77ca5d1c81dcf785c1351ce5dbd3be114728
Manali <manalivaghani1813@gmail.com>
2021-07-03 01:27:28 -0400

    second commit

--- src/main/java/com/example/CRUDOperation/CrudOperationApplication.java
+++ src/main/java/com/example/CRUDOperation/CrudOperationApplication.java
@@ -2,8 +2,10 @@ package com.example.CRUDOperation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class CrudOperationApplication {
 
 	public static void main(String[] args) {
